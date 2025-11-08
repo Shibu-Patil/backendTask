720c9ff0e3517c65f9b7d0a6b4985e1120b236ae
Shibu-Patil <patilshibu@gmail.com>
2025-11-08 17:22:16 +0530

    remove rmail

--- models/Student.js
+++ models/Student.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { 
-    type: String, 
-    required: true, 
+    type: String,  
     unique: true, 
     trim: true, 
     lowercase: true, 
